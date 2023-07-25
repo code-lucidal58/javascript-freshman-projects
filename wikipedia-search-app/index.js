9b3a51c81767e149e93f4fa55a1fe4b58488600e
code-lucidal58 <aanisha.mishra05@gmail.com>
2023-07-25 21:57:33 +0530

    receive serch query string

--- wikipedia-search-app/index.js
+++ wikipedia-search-app/index.js
@@ -1,4 +1,12 @@
 function afterLoad(){
-
+    const form = document.querySelector('.js-search-form');
+    form.addEventListener('submit', handleSubmit);
+    function handleSubmit(event) {
+        // prevent page from reloading when form is submitted
+        event.preventDefault();
+        const inputValue = document.querySelector('.js-search-input').value;
+        const searchQuery = inputValue.trim();
+        console.log(searchQuery);
+    }
 }
 window.onload = afterLoad;
