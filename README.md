92031bdc966a44924fe672e5f44f5032e8930bb2
code-lucidal58 <aanisha.mishra05@gmail.com>
2023-07-25 21:13:36 +0530

    add loader and disable button during fetch

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 Ref: https://freshman.tech/random-quote-machine/
 
+This webpage fetched a quote from What Does Trump Think API, which generates a random quote,
+and shows it. It has a loading animation to show the process of fetching. And, during the fetch process,
+the "get quote" button is disabled. This webpage also gives an option to tweet the shown quote to Twitter.
+
 ## APIs used
 * https://whatdoestrumpthink.com/api-docs/index.html
-
+* https://tobiasahlin.com/spinkit/ For the loading animation
--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 function afterLoad(){
+    const spinner = document.querySelector('#js-spinner');
     const newQuoteButton = document.querySelector('#js-new-quote');
     newQuoteButton.addEventListener('click', getQuote);
     const endpoint = 'https://api.whatdoestrumpthink.com/api/v1/quotes/random';
     async function getQuote(){
+        spinner.classList.remove('hidden');
+        newQuoteButton.disabled = true;
         try {
             const response = await fetch(endpoint);
             if (!response.ok){
                 throw Error(response.statusText);
             }
             const json = await response.json();
-            console.log(json.message);
+            displayQuote(json.message);
         } catch (err){
             console.log(err);
             alert('Failed to fetch new quote');
+        } finally {
+            newQuoteButton.disabled = false;
+            spinner.classList.add('hidden');
         }
     }
 
