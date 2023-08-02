8385511b7672c4e2571c8e00ac0f0485cfa4a731
code-lucidal58 <aanisha.mishra05@gmail.com>
2023-08-03 00:58:13 +0530

    add items to todo list, no UI

--- todo-app/index.js
+++ todo-app/index.js
@@ -1,4 +1,26 @@
-function afterLoad(){
-
+function afterLoad() {
+    let todoItems = [];
+    function addTodo(text) {
+        const todo = {
+            text,
+            checked: false,
+            id: Date.now() // ms elapsed since 1 Jan, 1970
+        }
+        todoItems.push(todo);
+        console.log(todoItems);
+    }
+    const  form = document.querySelector('.js-form');
+    form.addEventListener('submit', event => {
+        //prevent page refresh on form submission
+        event.preventDefault();
+        const input = document.querySelector('.js-todo-input');
+        const text = input.value.trim();
+        if (text!=='') {
+            addTodo(text);
+            input.value = '';
+            input.focus();
+        }
+    });
 }
+
 window.onload = afterLoad;
