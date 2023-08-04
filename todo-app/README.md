eb85925ed288e4551963959f187bcfc744414770
code-lucidal58 <aanisha.mishra05@gmail.com>
2023-08-05 01:16:41 +0530

    render todo items to UI

--- todo-app/README.md
+++ todo-app/README.md
@@ -9,7 +9,7 @@ This webpage is a basic todo list app. A user can add a task, mark it complete o
 * https://www.mediawiki.org/wiki/API:Search Wikipedia Search API
 
 ## JavaScript concepts learnt
-* 
+* `data-*` for custom data attributes on HTML elements
 
 ## TODO
 * 
--- todo-app/index.js
+++ todo-app/index.js
@@ -1,5 +1,22 @@
 function afterLoad() {
     let todoItems = [];
+
+    function renderTodo(todo) {
+        const list = document.querySelector('.js-todo-list');
+        const isChecked = todo.checked ? 'done' : '';
+        const node = document.createElement("li");
+        node.setAttribute('class', `todo-item ${isChecked}`);
+        node.setAttribute('data-key', todo.id);
+        node.innerHTML = `
+            <input type="checkbox" id="${todo.id}"/>
+            <label for="${todo.id}" class="tick js-tick"></label>
+            <span>${todo.text}</span>
+            <button class="delete-todo js-delete-todo">
+            <svg><use href="#delete-icon"></use></svg>
+            </button>`;
+        list.append(node);
+    }
+
     function addTodo(text) {
         const todo = {
             text,
@@ -7,15 +24,16 @@ function afterLoad() {
             id: Date.now() // ms elapsed since 1 Jan, 1970
         }
         todoItems.push(todo);
-        console.log(todoItems);
+        renderTodo(todo);
     }
-    const  form = document.querySelector('.js-form');
+
+    const form = document.querySelector('.js-form');
     form.addEventListener('submit', event => {
         //prevent page refresh on form submission
         event.preventDefault();
         const input = document.querySelector('.js-todo-input');
         const text = input.value.trim();
-        if (text!=='') {
+        if (text !== '') {
             addTodo(text);
             input.value = '';
             input.focus();
